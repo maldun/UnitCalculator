6b536a68d6dc579daefd1ccd265539429e57127e
Stefan Reiterer <stefan.reiterer@magnasteyr.com>
2013-10-01 08:50:13 +0200

    changed license preamble

--- unit_tests.py
+++ unit_tests.py
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 #
-# Copyright (c) 2013 Stefan Reiterer  - stefan.reiterer@magnasteyr.com or maldun.finsterschreck@gmail.com
+# Copyright (c) 2013 Stefan Reiterer  -  maldun.finsterschreck@gmail.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
